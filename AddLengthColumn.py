391dd423afcfb834d1af0af4b22c59ad159193e6
Mats Andersson <mats.olof.andersson@gmail.com>
2023-09-14 14:02:33 +0200

    SyntaxErrorStruggle (interpreter?)

--- AddLengthColumn.py
+++ AddLengthColumn.py
@@ -1,1 +1,8 @@
 #Assignment
+#def addColumnLength(source):
+#    print(f"Added length column to {source}")
+
+#source_file = open("/source data/brca_cnvs_tcga-1.csv", "r")
+print("Hello")
+#print(f"Data type: {type(source_file)}")
+#source_file.close()
