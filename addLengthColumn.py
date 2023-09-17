32f1ce1f565b8ca90ea4d73b8aeb86171ff4f0f5
Mats Andersson <mats.olof.andersson@gmail.com>
2023-09-17 18:33:39 +0200

    Renamed folders. Removed #.

--- addLengthColumn.py
+++ addLengthColumn.py
@@ -1,8 +1,9 @@
-#Assignment
-#def addColumnLength(source):
-#    print(f"Added length column to {source}")
+#Assignment 2
+def addColumnLength(source):
+    print(f"Added length column to {source}")
 
-#source_file = open("/source data/brca_cnvs_tcga-1.csv", "r")
+source_file = open("Assignment_2/source/brca_cnvs_tcga-1.csv", "r")
 print("Hello")
-#print(f"Data type: {type(source_file)}")
-#source_file.close()
+print(f"Data type: {type(source_file)}")
+addColumnLength(source_file)
+source_file.close()
