388fbb65005e5a3fb35c47b89d7c9d949fece0cf
Mats Andersson <mats.olof.andersson@gmail.com>
2023-09-17 19:51:33 +0200

    In progress

--- addLengthColumn.py
+++ addLengthColumn.py
@@ -1,9 +1,14 @@
 #Assignment 2
+import csv
+
 def addColumnLength(source):
     print(f"Added length column to {source}")
 
-source_file = open("Assignment_2/source/brca_cnvs_tcga-1.csv", "r")
-print("Hello")
-print(f"Data type: {type(source_file)}")
-addColumnLength(source_file)
-source_file.close()
+with (open("Assignment_2/source/brca_cnvs_tcga-1.csv", "r") as file):
+    data = csv.DictReader(file)
+    for row in data:
+        for cell in row['loc.start']:
+            print(f"cell:{cell}")
+#print(f"Data type: {type(source_file)}")
+#addColumnLength(source_file)
+#source_file.close()
