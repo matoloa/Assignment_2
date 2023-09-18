772bb9290251f918f8aaf48e9eba74e917868427
Mats Andersson <mats.olof.andersson@gmail.com>
2023-09-18 17:25:04 +0200

    Full version

--- lengthen.py
+++ lengthen.py
@@ -1,10 +1,12 @@
-#SC0039_Exercise_MA, Mats Andersson
+# SC0039_Exercise_MA, Mats Andersson. Should run with just "python lengthen"; output from source in output folder.
+# For more details, please examine README.md, accessible by "python lengthen ?"
 import csv
 import os
 import sys
 
+
 def fileOK(file):
-    # Attempts to create the output file, or raise an error if the name is invalid
+    # Attempts to create a file, or raises an error. Used to detect invalid paths.
     try:
         with open(file, 'w'):
             pass
@@ -12,15 +14,17 @@ def fileOK(file):
     except OSError:
         return False
 
+
 def writeSeqLength(source=None, output=None, suffix="-seq_length", inclusive=False):
     '''
-        Reads a .csv file that must contain the columns 'loc.end' and 'loc.start', containing data that can be read as integers.
-        Writes a new .csv file that also contains a column 'sec_length', containing each row's (loc.end - loc.start), in the "output" subfolder.
+        Reads a .csv file that must have the columns 'loc.end' and 'loc.start', which must contain data that can be parsed as integers.
+        Writes a new .csv file, by default in the "output" subfolder, that also has the column 'sec_length', containing each row's (loc.end - loc.start).
         
         args:
             source = csv to read from, defaults to brca_cnvs_tcga-1.csv in the source folder
             output = filename of the output file (in the output subfolder). Defaults to same as source filename
-            suffix = appended to filename to facilitate separation from source file. Defaults to -seq_length
+            suffix = appended to a default filename before the extension, to facilitate distinction from source file. Defaults to -seq_length.
+                suffix is ignored if the function is also passed an explicit output path
             inclusive is a manual override to prevent off-by-one error:
                 True:  assumes loc points to nucleotides (i.e. loc.start == loc.end should be length 1)
                 False: asummes loc points the space between them (in which case loc.start == loc.end is length 0)
@@ -28,46 +32,62 @@ def writeSeqLength(source=None, output=None, suffix="-seq_length", inclusive=Fal
     '''
     # write arguments, mostly for debug purposes
     print(f"writeSeqLength(source={source}, output={output}, suffix={suffix}, inclusive={inclusive})")
-    # if no source is supplied, default
+    # if no source is supplied, create a default path
     if source is None or source == "":
         source = "source/brca_cnvs_tcga-1.csv"
+
     # if there's a source file, open it
     if not os.path.exists(source):
         raise FileExistsError("Source file not found")
     with (open(source, "r") as source_file):
-        source_reader = csv.DictReader(source_file)
-        headers = source_reader.fieldnames + ['sec_length'] # add header
-        # if no output is supplied, build a default folder and filename
-        if output is None or output == "":
+        source_reader = csv.DictReader(source_file) #get headers from source file
+        headers = source_reader.fieldnames + ['sec_length'] # create new header
+
+        # if no output is supplied, build default folder and filename
+        if output is None or output == "": # if there's no output argument, create a default
             output_dir = "output/"
-            if not os.path.exists(output_dir):
+            if not os.path.exists(output_dir): # ensure the default output directory exists
                 os.mkdir(output_dir)
-            output, csv_ext = os.path.splitext(os.path.basename(source))
-            print(f"output1: {output}")
-            output = output_dir + output + suffix + ".csv"
-            print(f"output2: {output}")
+            output, csv_ext = os.path.splitext(os.path.basename(source)) # strip extension from output, so that suffix can be added
+            output = output_dir + output + suffix + csv_ext # compile final default output (path)
+
         # if output is a valid path, open it for writing
         if not fileOK(output):
             raise SyntaxError("Output is not a valid filename; incorrect path or suffix?")
         output_file = open((output), "w")
-        output_writer = csv.DictWriter(output_file, fieldnames=headers)
-        output_writer.writeheader()
+        output_writer = csv.DictWriter(output_file, fieldnames=headers) # set new headers
+        output_writer.writeheader() # write new headers
+
         # calculate and append sec_length column
         for row in source_reader:
             sec_length = (int(row['loc.end']) - int(row['loc.start'])) 
             if(inclusive):
-                sec_length += 1
-            row['sec_length'] = str(sec_length)
+                sec_length += 1 # counter potential off-by-one error
+            row['sec_length'] = str(sec_length) # convert to string for storage
             output_writer.writerow(row)
+
         output_file.close()
+    print(f"A new column 'sec_length' added to {output}.")
+
+
+def printText(text_file):
+    # prints a textfile (row by row) in current working directory, or raises an error if it's not found
+    if not os.path.exists(text_file):
+        raise FileExistsError(f"Failed to locate {text_file}; lengthen.py not in expected environment?")
+    else:
+        with open(text_file, "r") as printer:
+            for row in printer:
+                print(row)        
+
 
 def runByArgs(args):
-    # calls writeSewLength with up to four positional arguments
+    # calls writeSeqLength with up to four positional arguments
     if "?" in args:
-        print("README.md") #TODO: acutally print it
+        printText("README.md")
     else:
-        if 4 < len(args):
-            raise SyntaxError("Too many arguments; please supply, by position: [source output suffix inclusive(boolean)], or ? for help.")
+        if 4 < len(args): # ensure allowed number of arguments
+             raise SyntaxError("Too many arguments; please supply, by position: [source output suffix inclusive(boolean)], or ? for help.")
         writeSeqLength(*args)
 
-runByArgs(sys.argv[1:])
+
+runByArgs(sys.argv[1:]) # execute
